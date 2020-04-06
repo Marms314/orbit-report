3d3717956d569d0ce9c4931ac20e86b5af38b038
Marms <maryarmstrong314@gmail.com>
2020-04-06 10:56:56 -0500

    added orbit list compnent and satellite class. fetched satellite data

---
+++ src/app/satellite.spec.ts
@@ -0,0 +1,8 @@
+import { Satellite } from './satellite';
+import { Assert } from 'Assert';
+
+describe('Satellite', () => {
+  it('should create an instance', () => {
+    expect(new Satellite()).toBeTruthy();
+  });
+});
